8fe3adff503f6fa4dd32dcfc86f6e2994d762acc
Josh Cardif <josh.cardif14@outlook.com>
2017-12-13 11:45:52 +0300

    Add project files.

---
+++ CodeFirebaseUser/AflameUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace CodeFirebaseUser
+{
+    public class AflameUser
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Residence { get; set; }
+        public string PhoneNo { get; set; }
+        public string Email { get; set; }
+        public string Occupation { get; set; }
+        public string Cell { get; set; }
+        public string Minisry { get; set; }
+        public string PhotoUrl { get; set; }
+    }
+}
---
+++ CodeFirebaseUser/FirebaseUserCode.cs
@@ -0,0 +1,54 @@
+﻿using Android.Widget;
+using Firebase.Auth;
+using System;
+using Firebase.Database;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Firebase.Xamarin.Database;
+using Firebase.Xamarin.Database.Query;
+
+namespace CodeFirebaseUser
+{
+    public class FirebaseUserCode
+    {
+        public string Email, Name;
+        private Android.Net.Uri PhotoUrl;
+        public FirebaseUserCode()
+        {
+            var user = FirebaseAuth.Instance.CurrentUser;
+
+            if (user != null)
+            {
+                Name = user.DisplayName;
+                PhotoUrl = user.PhotoUrl;
+                Email = user.Email;
+            }
+        }
+        public async void GetUserDetails(FirebaseClient firebase, string child, AflameUser user)
+        {
+            var items = await firebase
+                .Child(child)
+                .Child(Email)
+                .OnceAsync<AflameUser>();
+            foreach (var item in items)
+            {
+                user = new AflameUser()
+                {
+                    Name=item.Object.Name,
+                    ID=item.Key,
+                    Cell=item.Object.Cell,
+                    Email=item.Object.Email,
+                    Minisry=item.Object.Minisry,
+                    PhoneNo=item.Object.PhoneNo,
+                    Occupation=item.Object.Occupation,
+                    Password=item.Object.Password,
+                    PhotoUrl=item.Object.PhotoUrl,
+                    Residence=item.Object.Residence,
+                    Username=item.Object.Username,                 
+                };
+            }
+                      
+        }
+    }
+}
